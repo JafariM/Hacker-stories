ebbb66d250a4c84337587ced4acb2bf6a45233ef
Masooma <masuma.jafari@gmail.com>
2023-05-04 12:58:47 -0700

    use acync function, reduce hook and impossible state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import * as React from 'react';
 
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+// instead of getting stories form array, we will get if from an API
+const getAsyncStories = () =>
+  new Promise((resolve) =>
+    setTimeout(
+      () => resolve({ data: { stories: initialStories } }),
+      2000
+    )
+  );
+
 const App = () => {
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
+
   //set a custom hook that syn value of search and local storage 
   const useSemiPersistentState = (key, initialState) => {
     const [value, setValue] = React.useState(
@@ -33,14 +44,77 @@ const App = () => {
     return [value, setValue];
   }
 
+  //use a reducer function to better manage the state of stories
+  const storiesReducer = (state, action) => {
+    switch (action.type) {
+      case 'STORIES_FETCH_INIT':
+        return {
+          ...state,
+          isLoading: true,
+          isError: false,
+        }
+      case 'STORIES_FETCH_SUCCESS':
+        return {
+          ...state,
+          isLoading: false,
+          isError: false,
+          data: action.payload,
+        }
+      case 'STORIES_FETCH_FAILURE':
+        return {
+          ...state,
+          isLoading: false,
+          isError: true,
+        }
+
+      case 'REMOVE_STORY':
+        return {
+          ...state,
+          data: state.data.filter(
+            (story) => action.payload.objectID !== story.objectID
+          ),
+        };
+
+      default:
+        throw new Error();
+    }
+  }
+
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
+  //use a reducer hook instead of state hook for stories state
+  const [stories, dispatchStories] = React.useReducer(storiesReducer,
+    { data: [], isLoading: false, isError: false }
+  );
+
+  // use a side effect to display stories from promise
+  React.useEffect(() => {
+    dispatchStories({ type: 'STORIES_FETCH_INIT' });
+
+    getAsyncStories()
+      .then((result) => {
+        dispatchStories({
+          type: 'STORIES_FETCH_SUCCESS',
+          payload: result.data.stories,
+        });
+      })
+      .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
+  }, []);
+
+  //filter stories and remove the ones that does not meet the condition
+  const handleRemoveStory = (item) => {
+    dispatchStories({
+      type: 'REMOVE_STORY',
+      payload: item,
+    });
+  };
+
   //received the value of search input and update the setSearchTerm function
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
 
   // filter stories array by value reciving from search input 
-  const searchedStories = stories.filter((story) =>
+  const searchedStories = stories.data.filter((story) =>
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
@@ -56,7 +130,14 @@ const App = () => {
         <strong>Search: </strong>  {/* a component composition that can be accessed as children in InputWithLabel */}
       </InputWithLabel>
       <hr />
-      <List list={searchedStories} />
+      {stories.isError && <p>Something went wrong ...</p>}
+      {stories.isLoading ? <p>IS loading..</p> :
+        <List
+          list={searchedStories}
+          onRemoveItem={handleRemoveStory}
+        />
+      }
+
     </div>
   );
 };
@@ -80,23 +161,31 @@ const InputWithLabel = ({ id, value, onInputChange, type = 'text', children }) =
 
 // );
 
-const List = ({ list }) => (
+const List = ({ list, onRemoveItem }) => (
   <ul>
-    {list.map(({ objectID, ...item }) => (
-      // sending item as spread operator to item component
-      <Item key={objectID} {...item} />
+    {list.map((item) => (
+      <Item
+        key={item.objectID}
+        item={item}
+        onRemoveItem={onRemoveItem}
+      />
     ))}
   </ul>
 );
 
-const Item = ({ title, url, author, num_comments, points }) => (
+const Item = ({ item, onRemoveItem }) => (
   <li>
     <span>
-      <a href={url}>{title}</a>
+      <a href={item.url}>{item.title}</a>
     </span>
-    <span>{author}</span>
-    <span>{num_comments}</span>
-    <span>{points}</span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+    <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        Dismiss
+      </button>
+    </span>
   </li>
 );
 
